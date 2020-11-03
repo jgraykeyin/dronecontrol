2700a34cdb9ff98a35309d2ca272a085571264f1
jgraykeyin <justin.gray@keyin.com>
2020-11-03 10:54:28 -0330

    Added an initial loop and two inputs

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+# import will go here
+
+# Setup a loop here to start taking inputs
+while True:
+    
+    # First input to ask for the command.
+    # TODO: Setup input validation
+    user_command = input("Command: ")
+
+    # Second input to ask for a number value in centimeters
+    # TODO: Setup input validation
+    user_cm = input("Centimeters: ")
+
