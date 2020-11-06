4ef8e9a0fe5f5e9c8864759c56d9cf8ee028783c
jgraykeyin <justin.gray@keyin.com>
2020-11-06 17:46:57 +0000

    Added flip function

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import tello_abridged
 # TODO: Track if drone is in the air or not
 
 #Connect to the drone
-t = tello_abridged.Tello()
-t.connect_and_initialize()
+#t = tello_abridged.Tello()
+#t.connect_and_initialize()
 
 # List of valid commands the tello will accept
 # Takeoff and land are in their own list because they don't need arguments
-standard_commands = ["up","down","left","right","forward","back","cw","ccw"]
+standard_commands = ["up","down","left","right","forward","back","cw","ccw","flip"]
 noarg_commands = ["takeoff","land"]
 
-# Start a loop here to start taking inputs
+
 while True:
     
     # Start the main loop and run until the user quits
@@ -40,9 +40,27 @@ while True:
         # Validation for most commands must be between 20 and 500
         # Validation for cw and ccw comands must be between 1 and 360
         if user_command == "cw" or user_command == "ccw":
-            user_num = int(input("Degrees: "))
+            
+            while True:
+                user_num = int(input("Degrees: "))
+                
+                if user_num >= 1 and user_num <= 360:
+                    break
+                else:
+                    print("Please enter a number between 1 and 360")
+        
+        elif user_command == "flip":
+            user_num = input("Direction (l,r,f): ")
+                    
         else:
-            user_num = int(input("Centimeters: "))
+                
+            while True:
+                user_num = int(input("Centimeters: "))
+                
+                if user_num >= 2 and user_num <= 500:
+                    break
+                else:
+                    print("Please enter a number between 20 and 500")
         
     # Make sure we have a condition to quit the program.
     # TODO: Look into issue with height & cm travelled
