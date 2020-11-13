d58604cc047fed60a526d5d0565b46dddeb6e3c8
jgraykeyin <justin.gray@keyin.com>
2020-11-13 09:21:23 -0330

    Added Takeoff and Land commands to GUI

--- gui.py
+++ gui.py
@@ -1,20 +1,32 @@
+# Drone Control Py by Drone Team 2-1
+# Sprint Week Project #1, November 2020
+# Python program for sending user commands to the Tello Drone
+
+import tello_abridged
 import PySimpleGUI as sg
 
-sg.theme("DarkAmber")
+#Connect to the drone
+t = tello_abridged.Tello()
+t.connect_and_initialize()
 
-layout = [  [sg.Text("Drone Control")],
-            [sg.Button("Takeoff"),sg.Button("Land")],
-            [sg.Button("Up"),sg.Button("Down")],
-            [sg.Button("Left"),sg.Button("Right")]]
+sg.theme("DarkAmber")
+# Create the layout for the GUI 
+layout = [ [sg.Button('Takeoff',font=("Helvetica", 18))],
+           [sg.Button('Land',font=("Helvetica", 18))],
+           [sg.Button('Cancel',font=("Helvetica",18))]  ]
 
 # Create the window
 window = sg.Window('Drone Control',layout)
 
-# Event loop to process events and get inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == "Cancel":
-        break
-    print("You entered:", values[0])
 
-window.close()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    
+    elif event == 'Takeoff':
+        t.send_command('takeoff')
+
+    elif event == 'Land':
+        t.send_command('land')
+
--- main.py
+++ main.py
@@ -1,45 +1,105 @@
+# Drone Control Py by Drone Team 2-1
+# Sprint Week Project #1, November 2020
+# Python program for sending user commands to the Tello Drone
+
 import tello_abridged
+
+# TODO: Track if drone is in the air or not
 
 #Connect to the drone
 t = tello_abridged.Tello()
 t.connect_and_initialize()
 
 # List of valid commands the tello will accept
-# TODO: enter all the commands into this list
-valid_commands = []
+# Takeoff and land are in their own list because they don't need arguments
+standard_commands = ["up","down","left","right","forward","back","cw","ccw","flip"]
+noarg_commands = ["takeoff","land"]
 
-# Start a loop here to start taking inputs
-while True:
+# Set the flying variable to True once it takes off so we can know if it's in the air or not
+flying = False
+run_program = True
+
+while run_program:
     
-    # First input to ask for the command.
+    # Start the main loop and run until the user quits
     # TODO: Setup input validation
-    # We'll need to make sure it's one of the valid commands, ie takeoff
-    # Perhaps make a list of commands and check against that?
-    user_command = input("Command: ")
-
+    print("--* Drone Control Py *--")
+    while True:
+        
+        # Print the list of commands and wait for the user's input
+        print("Available Commands: {} {}".format(noarg_commands,standard_commands))
+        print ("Q to quit program")
+        user_command = input("Command: ")
+        
+        if user_command.lower() == "q" and flying == False:
+            run_program = False
+            break
+        elif user_command.lower() == "q" and flying == True:
+            print("Please land before quitting")
+            
+        # Make sure the user enters a valid command by checking against a list of commands
+        elif user_command.lower() in standard_commands or user_command.lower() in noarg_commands:
+            break
+        else:
+            print("Please input a valid command.")
+    
+    if run_program == False:
+        break
+    
     # We don't need to prompt user for a second value if it's taking off or landing
-    if user_command.lower() != "takeoff":
+    if user_command.lower() not in noarg_commands:
         # Second input to ask for a number value in centimeters
-        # TODO: Setup input validation
         # Validation for most commands must be between 20 and 500
         # Validation for cw and ccw comands must be between 1 and 360
-        user_cm = int(input("Centimeters: "))
+        if user_command == "cw" or user_command == "ccw":
+            
+            while True:
+                
+                try:
+                    user_num = int(input("Degrees: "))
+                
+                    if user_num >= 1 and user_num <= 360:
+                        break
+                    else:
+                        print("Please enter a number between 1 and 360")
+                except:
+                    print("Please enter a valid number.")
         
-    # Setup if statements to handle user commands.
+        elif user_command == "flip":
+            user_num = input("Direction (l,r,f): ")
+                    
+        else:
+                
+            while True:
+                try:
+                    user_num = int(input("Centimeters: "))
+                
+                    if user_num >= 20 and user_num <= 500:
+                        break
+                    else:
+                        print("Please enter a number between 20 and 500")
+                except:
+                    print("Please input a valid number")
+        
     # Make sure we have a condition to quit the program.
-    # Commands we must support: takeoff, up, down, left, right, forward, back, cw, ccw, land
-    # 
     # TODO: Look into issue with height & cm travelled
-    if user_command.lower() == "takeoff":
+    # This section will handle the user's commands and send them to the drone using t.send_command
+    # 
+    # If the command is a takeoff or landing, it will send a command without an argument
+    if user_command.lower() in noarg_commands:
         t.send_command(user_command.lower())
+        #print(user_command.lower())
         
-    elif user_command.lower() == "up":
-        # Creating a new variable so we can combine user_command and user_cm into a single string
-        command = user_command.lower() + " " + str(user_cm)
+        if user_command.lower() == "takeoff":
+            flying = True
+        elif user_command.lower() == "land":
+            flying = False
+        
+    # If the command is any of the other standard commands, it will send command + argument
+    elif user_command.lower() in standard_commands:
+        # Creating a new variable so we can combine user_command and user_num into a single string
+        command = user_command.lower() + " " + str(user_num)
         # Then we pass that combined command to the tello
+        #print(command)
         t.send_command(command)
-
-    elif user_command.lower() == "down":
-        command = user_command.lower() + " " + str(user_cm)
-        t.send_command(command)
-        
+        flying = True
